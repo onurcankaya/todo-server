ab66c4f2d6f3d3e0c5948b449c3d676807d4b4b6
Onur Çankaya <onur.eren.cankaya@gmail.com>
2024-08-28 15:56:58 +0200

    implement delete todo route

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ app.post("/todos", async (req, res) => {
       [title, description]
     );
 
-    res.json(newTodo.rows[0]);
+    res.status(200).json(newTodo.rows[0]);
   } catch (error) {
     console.error((error as Error).message);
     res.status(500).send("Error creating todo");
@@ -30,10 +30,34 @@ app.post("/todos", async (req, res) => {
 app.get("/todos", async (req, res) => {
   try {
     const allTodos = await pool.query("SELECT * from todos");
-    res.json(allTodos.rows);
+    res.status(200).json(allTodos.rows);
   } catch (error) {
     console.log((error as Error).message);
     res.status(500).send("Error fetching todos");
+  }
+});
+
+// route to delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const result = await pool.query(
+    "DELETE FROM todos WHERE id = $1 RETURNING *",
+    [id]
+  );
+
+  if (result.rowCount === 0) {
+    return res.status(404).json({ message: "Todo not found" });
+  }
+
+  res
+    .status(200)
+    .json({ message: "Todo deleted successfully", todo: result.rows[0] });
+
+  try {
+  } catch (error) {
+    console.error((error as Error).message);
+    res.status(500).json({ message: "Error deleting todo" });
   }
 });
 
