969b39462ca6425935ad077d9836ae8fc7855287
Onur Çankaya <onur.eren.cankaya@gmail.com>
2024-08-29 15:17:09 +0200

    implement update todo route

--- src/index.ts
+++ src/index.ts
@@ -64,3 +64,34 @@ app.delete("/todos/:id", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+// route to update a todo
+app.put("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, description, is_completed } = req.body;
+
+  try {
+    const result = await pool.query(
+      `UPDATE todos
+      SET title = COALESCE($1, title),
+          description = COALESCE($2, description),
+          is_completed = COALESCE($3, is_completed),
+          updated_at = CURRENT_TIMESTAMP
+      WHERE id = $4
+      RETURNING *
+      `,
+      [title, description, is_completed, id]
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "Todo updated successfully", todo: result.rows[0] });
+  } catch (error) {
+    console.error("Error updating todo:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
